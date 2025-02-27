4c327100a56e849eb3bb93d6cb29c3b5564cdaae
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-28 00:59:28 +0530

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Deepjyoti-Sarmah/fast-api
+
+go 1.23.0
+
+require (
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
---
+++ store/db.go
@@ -0,0 +1,26 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/Deepjyoti-Sarmah/fast-api/config"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDb(conf *config.Config) (*sql.DB, error) {
+	dsn := conf.DatabaseUrl()
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database connection: %w", err)
+	}
+	return db, nil
+}
