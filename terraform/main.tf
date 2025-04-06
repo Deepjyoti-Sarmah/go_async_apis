69943ca9fbd3e7795c24f08c9ce5999ce71b84df
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-04-06 20:46:37 +0530

    adding terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+variable "aws_secret_access_key" {
+type = string
+}
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_default_region" {
+  type = string
+}
+
+variable "s3_bucket" {
+  type = string
+}
+
+variable "sqs_queue" {
+  type = string
+}
+
+
+provider "aws" {
+
+  access_key                  = var.aws_access_key_id
+  secret_key                  = var.aws_secret_access_key
+  region                      = var.aws_default_region
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
+
+resource "aws_s3_bucket" "reports-s3-bucket" {
+  bucket = var.s3_bucket
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = var.sqs_queue
+  delay_seconds             = 5
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
